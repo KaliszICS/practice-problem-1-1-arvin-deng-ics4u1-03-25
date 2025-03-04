7b63d92b54628e1ba0e138bd775568dcacd04300
arvin-deng-ics4u1-03-25 <arvin.deng@student.tdsb.on.ca>
2025-03-04 02:47:39 +0000

    1.1 answers

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,24 +4,34 @@ public class PracticeProblem {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static int sum2D(int[][] array) {
+		int sum = 0;
+
+		for  (int row = 0; row < array.length; row++){
+			for (int col = 0; col < array[row].length; col++) {
+				sum += array[row][col];
+			}
+		}
+		return sum;
 	}
 
-	public static void q2() {
-		//Write question 2 code here
+	public static int sumRow(int[][] array2, int row) {
+		int sumrow = 0;
+
+		for (int col = 0; col < array2[row].length; col++) {
+			sumrow += array2[row][col];
+		}
+		return sumrow;
 	}
 
-	public static void q3() {
-		//Write question 3 code here
-	}
+	public static int sumColumn(int[][] array3, int col) {
+		int colsum = 0;
+		
+		for (int row = 0; row < array3.length; row++) {
+			colsum += array3[row][col];
+		}
 
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
+		return colsum;
 	}
 
 }
